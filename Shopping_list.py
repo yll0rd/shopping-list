cefec37538c4cf19156a64a58b3248ddf82b2aa1
LeoYoumbi <Leoyoumbi83@gmail.com>
2023-03-03 12:14:46 +0100

    'Modifying the way the list is viewed'

--- Shopping_list.py
+++ Shopping_list.py
@@ -12,21 +12,27 @@ while True: #for an infinite loop
     # Defining the different options
     match n:
         case 1:
-            added = input("Enter the name of the element to add: ")
             with open("Items_list.txt", "r") as f:
                 cont = f.read()
+            added = input("Enter the name of the element to add: ")
+            # Checking if the element already exists
+            present = None
+            if added.capitalize() in cont.splitlines(): present = True
+            while present == True:
+                added = input(f"> The element '{added.capitalize()}' is already in the list.\nEnter another one: ")
+                if added.capitalize() not in cont.splitlines(): present = False
             with open("Items_list.txt", "a") as f:
                 if cont == "":
-                    f.write(added) #If the text file is originally empty, the added element won't go the next line
+                    f.write(added.capitalize()) #If the text file is originally empty, the added element won't go the next line
                 else:
-                    f.write(f"\n{added}") #If the elements in the file, the element will be added in the next line
+                    f.write(f"\n{added.capitalize()}") #If the elements in the file, the element will be added in the next line
         
         case 2:
             rmv = input("Enter the name of the element to remove: ")
             with open("Items_list.txt", "r") as f:
                 liste = f.read().splitlines() #reading the text while converting it into a list
-                if rmv in liste:
-                    liste.remove(rmv)
+                if rmv.capitalize() in liste:
+                    liste.remove(rmv.capitalize())
                 else:
                     print(f"The item '{rmv}' is not in the shopping list")
                 liste = "\n".join(liste) #Joining the diff elements of the list while separating with a \n
@@ -36,11 +42,15 @@ while True: #for an infinite loop
 
         case 3:
             with open("Items_list.txt", "r") as f:
-                content = f.read()
-                if content == "":
-                    print("The list is already empty")
+                content = f.read().splitlines()
+                if content == []:
+                    print("The list is empty. Choose '1' to add an item.")
                 else:
-                    print(content)
+                    i = 1
+                    print("Below is the Items list: ")
+                    for item in content:
+                        print(f"{i} - {item}")
+                        i += 1
         case 4:
             with open("Items_list.txt", "r") as f:
                 conte = f.read()
@@ -53,4 +63,3 @@ while True: #for an infinite loop
                 print("The list is already empty")
             else:
                 print("The list has been emptied")
-    
